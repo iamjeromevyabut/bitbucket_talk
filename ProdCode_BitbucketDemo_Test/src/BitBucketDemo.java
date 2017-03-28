07cdd7a93aa5d44444efb4422af3b79f3f568d5d
Jerome Yabut <yabut.jerome.v@gmail.com>
2017-03-29 04:56:51 +0800

    [JIRATICKET-003] Initial Production Code

---
+++ ProdCode_BitbucketDemo_Test/src/BitBucketDemo.java
@@ -0,0 +1,9 @@
+
+public class BitBucketDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a bitbucket demonstration demo");
+	}
+
+}
